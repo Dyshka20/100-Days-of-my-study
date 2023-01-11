34b87be263e4a60dad21c3cdca313b27940688bb
Andrey <122141227+Dyshka20@users.noreply.github.com>
2023-01-12 00:09:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ print("Привет мой друг")
 #2.Ask the user for the city that they grew up in.
 city = (input("Где ты живещь педик?\n"))
 #3.Ask the user for the name of a pet.
-pate_name = (input("Как звать твоего питомца пидара?\n"))
+pate_name = (input("Как звать твоего питомца лох?\n"))
 #4.Combine the name of their city and pet and show them their band name.
 print("Имя вашей пидарской группы\n"+city+" "+pate_name)
-#5.Make sure the input cursor shows  on a new line:
-
-# Solution: https://replit.com/@appbrewery/band-name-generator-end
